a2b2450aa3158304ff76e481960ed8af83ebc2a7
Harden, Whitney <wharden@fusionalliance.com>
2019-11-19 12:23:55 -0500

    form validation and formating

---
+++ js/validation.js
@@ -0,0 +1,12 @@
+//basic validation
+/**
+ * mandatory fields are filled
+ */
+
+ function validateForm()
+ {
+     let nameInput = 1; //nameInput = the value of what is in the input for name
+     let emailInput = 1; //emailInput = the value of what is in the input for input
+ }
+
+//data format validation
